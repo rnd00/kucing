c6068607ec22ced9600eec5b1d54b872cbedfb10
rnd00 <rdiedan@gmail.com>
2022-06-13 17:45:08 +0900

    update: put all goroutine on main, try running

--- main.go
+++ main.go
@@ -2,37 +2,86 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 
 	"github.com/rnd00/kucing/download"
+	"github.com/rnd00/kucing/helpers"
 )
 
 func main() {
-	testingDownload()
+	// testingDownload()
+	testingDownloadUsingWorker()
 }
 
-func testingDownload() {
-	dataC := make(chan []byte)
-	errC := make(chan error)
+func testingDownloadUsingWorker() {
+	workerAmt := 2
+	jobsAmt := 12
 
-	go func() {
-		for i := 0; i < 20; i++ {
-			time.Sleep(time.Second / 2)
-			download.Routine(dataC, errC)
+	var wg sync.WaitGroup
+	jobs := make(chan int, jobsAmt)
+
+	// open up subthread
+	for i := 0; i < workerAmt; i++ {
+		wg.Add(1)
+		logs := make(chan *helpers.Log)
+		go worker(i+1, jobs, logs)
+		go logger(i+1, logs, &wg)
+	}
+
+	// assign some jobs
+	for i := 0; i < jobsAmt; i++ {
+		jobs <- i + 1
+	}
+	// close when done
+	close(jobs)
+
+	// put waitgroup to wait
+	wg.Wait()
+	procEndLog := helpers.NewLogInfo("[MAIN] All the channels are closed and workers are done.")
+	procEndLog.Print()
+}
+
+func worker(workerTag int, jobsC <-chan int, logsC chan<- *helpers.Log) {
+	defer close(logsC)
+
+	// while jobsC is not closed
+	for n := range jobsC {
+		// start log
+		startLogMsg := fmt.Sprintf("[WORKER %d] job %d\t| Start", workerTag, n)
+		startL := helpers.NewLogInfo(startLogMsg)
+		logsC <- startL
+
+		// get data
+		bytedata, err := download.Cat()
+		if err != nil {
+			errL := helpers.NewLogError(*helpers.NewError(err))
+			logsC <- errL
+
+			// skip this loop
+			continue
 		}
-	}()
+		// key := compare.MakeKey(bytedata)
+		// exist := cache.CheckExistence(key)
+		// if exist { log <- errors.New("data already exist"); return }
+		// helpers.WriteToFile(bytedata)
 
-	go func() {
-		for data := range dataC {
-			fmt.Println("[RETRIEVED]", time.Now().Format(time.RFC3339), len(data))
-		}
-	}()
+		// end log (debug)
+		endLogMsg := fmt.Sprintf("[WORKER %d] job %d\t| Retrieved with length %d", workerTag, n, len(bytedata))
+		endL := helpers.NewLogInfo(endLogMsg)
+		logsC <- endL
+	}
 
-	go func() {
-		for err := range errC {
-			fmt.Println("[ERROR]", time.Now().Format(time.RFC3339), err)
-		}
-	}()
+	stopLogMsg := fmt.Sprintf("[WORKER %d] jobs done, closing", workerTag)
+	stopL := helpers.NewLogInfo(stopLogMsg)
+	logsC <- stopL
+	return
+}
 
-	fmt.Scanln()
+func logger(workerTag int, c chan *helpers.Log, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for data := range c {
+		fmt.Println(data)
+	}
+	stopLoggerMsg := fmt.Sprintf("[LOGGER %d] worker is done, closing", workerTag)
+	fmt.Println(helpers.NewLogInfo(stopLoggerMsg))
 }
