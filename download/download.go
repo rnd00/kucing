6f3f5d98403a1be3a33d7f03c89b01d6635507b4
rnd00 <rdiedan@gmail.com>
2022-06-13 14:26:54 +0900

    update: download.Cat()

--- download/download.go
+++ download/download.go
@@ -1,1 +1,29 @@
 package download
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+const thiscatdoesnotexist string = "https://thiscatdoesnotexist.com"
+
+func Cat() ([]byte, error) {
+	var data []byte
+	resp, err := http.Get(thiscatdoesnotexist)
+	if err != nil {
+		return data, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return data, err
+		}
+		data = bodyBytes
+		return data, nil
+	}
+	return []byte{}, errors.New(fmt.Sprintf("HTTP Status Code returned was not expected; %+v", resp.StatusCode))
+}
