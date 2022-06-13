4f0e0dd871a24b9d1f93b980253bedfcd7c10b47
rnd00 <rdiedan@gmail.com>
2022-06-13 17:44:24 +0900

    update: simplify

--- helpers/log.go
+++ helpers/log.go
@@ -10,37 +10,56 @@ type severity int
 const (
 	debug severity = iota
 	info
-	warn
 	fatal
 )
 
 func (s severity) String() string {
-	a := []string{"Debug", "Info", "Warn", "Fatal"}
+	a := []string{"Debug", "Info", "Fatal"}
 	return a[s]
 }
 
-type log struct {
+type Log struct {
 	time     time.Time
 	severity severity
 	message  string
 }
 
-func NewLogMessage(s severity, msg string) *log {
-	return &log{
+func NewLogMessage(s severity, msg string) *Log {
+	return &Log{
 		time:     time.Now(),
 		severity: s,
 		message:  msg,
 	}
 }
 
-func (l *log) String() string {
+func NewLogDebug(msg string) *Log {
+	return &Log{
+		time:     time.Now(),
+		severity: debug,
+		message:  msg,
+	}
+}
+
+func NewLogInfo(msg string) *Log {
+	return &Log{
+		time:     time.Now(),
+		severity: info,
+		message:  msg,
+	}
+}
+
+func NewLogError(e err) *Log {
+	return &Log{
+		time:     time.Now(),
+		severity: fatal,
+		message:  e.String(),
+	}
+}
+
+func (l *Log) String() string {
 	return fmt.Sprintf("[%s] %s | %s", time.Now().Format(time.RFC3339), l.severity, l.message)
 }
 
-func (l *log) Print() {
+func (l *Log) Print() {
 	fmt.Println(l)
-}
-
-func RunLog() {
-	// add later
 }
