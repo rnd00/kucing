6c6558fb7a95128ad00d06654185eaf3a1752ef0
rnd00 <rdiedan@gmail.com>
2022-06-14 13:18:08 +0900

    update: delay 1s, download runs nicely, will do filesave later

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"sync"
+	"time"
 
+	"github.com/rnd00/kucing/cache"
+	"github.com/rnd00/kucing/compare"
 	"github.com/rnd00/kucing/download"
 	"github.com/rnd00/kucing/helpers"
 )
@@ -19,18 +23,22 @@ func testingDownloadUsingWorker() {
 
 	var wg sync.WaitGroup
 	jobs := make(chan int, jobsAmt)
+	cache := cache.NewCache()
 
 	// open up subthread
 	for i := 0; i < workerAmt; i++ {
 		wg.Add(1)
 		logs := make(chan *helpers.Log)
-		go worker(i+1, jobs, logs)
+		// spin up another thread
+		go worker(i+1, jobs, logs, cache)
 		go logger(i+1, logs, &wg)
 	}
 
 	// assign some jobs
 	for i := 0; i < jobsAmt; i++ {
 		jobs <- i + 1
+		// add sleep so that it has a delay
+		time.Sleep(time.Second)
 	}
 	// close when done
 	close(jobs)
@@ -41,7 +49,7 @@ func testingDownloadUsingWorker() {
 	procEndLog.Print()
 }
 
-func worker(workerTag int, jobsC <-chan int, logsC chan<- *helpers.Log) {
+func worker(workerTag int, jobsC <-chan int, logsC chan<- *helpers.Log, cache *cache.Cac) {
 	defer close(logsC)
 
 	// while jobsC is not closed
@@ -51,26 +59,52 @@ func worker(workerTag int, jobsC <-chan int, logsC chan<- *helpers.Log) {
 		startL := helpers.NewLogInfo(startLogMsg)
 		logsC <- startL
 
-		// get data
-		bytedata, err := download.Cat()
-		if err != nil {
-			errL := helpers.NewLogError(*helpers.NewError(err))
-			logsC <- errL
+		// make another loop here
+		// if fail then retry downloading
+		jobDone := false
+		attempt := 0
 
-			// skip this loop
-			continue
+		for !jobDone {
+			// retry attempt log
+			attempt += 1
+			retryAttemptLog := fmt.Sprintf("[WORKER %d] job %d\t| Attempt %d", workerTag, n, attempt)
+			retryL := helpers.NewLogInfo(retryAttemptLog)
+			logsC <- retryL
+
+			// get data
+			bytedata, err := download.Cat()
+			if err != nil {
+				errL := helpers.NewLogError(*helpers.NewError(err))
+				logsC <- errL
+				// retry this loop by skipping
+				continue
+			}
+
+			// how to restart the job when comparison failed?
+			key := compare.MakeKey(helpers.ToBase64Bytes(bytedata))
+			debugLog := fmt.Sprintf("[WORKER %d] job %d\t| generated key: %s", workerTag, n, key)
+			logsC <- helpers.NewLogDebug(debugLog)
+
+			exist := cache.CheckKey(key)
+			if exist {
+				errL := helpers.NewLogError(*helpers.NewError(errors.New("Same generated key existed in the cache")))
+				logsC <- errL
+				// retry this loop by skipping
+				continue
+			}
+
+			cache.SetKey(key)
+			// helpers.WriteToFile(bytedata)
+			jobDone = true
+			jobDoneMsg := fmt.Sprintf("[WORKER %d] job %d\t| done", workerTag, n)
+			jobDoneL := helpers.NewLogInfo(jobDoneMsg)
+			logsC <- jobDoneL
 		}
 
-		// how to restart the job when comparison failed?
-		// key := compare.MakeKey(bytedata)
-		// exist := cache.CheckExistence(key)
-		// if exist { log <- errors.New("data already exist"); return }
-		// helpers.WriteToFile(bytedata)
-
 		// end log (debug)
-		endLogMsg := fmt.Sprintf("[WORKER %d] job %d\t| Retrieved with length %d", workerTag, n, len(bytedata))
-		endL := helpers.NewLogInfo(endLogMsg)
-		logsC <- endL
+		// endLogMsg := fmt.Sprintf("[WORKER %d] job %d", workerTag, n)
+		// endL := helpers.NewLogInfo(endLogMsg)
+		// logsC <- endL
 	}
 
 	stopLogMsg := fmt.Sprintf("[WORKER %d] jobs done, closing", workerTag)
